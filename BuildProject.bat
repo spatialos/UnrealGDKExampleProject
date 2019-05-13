901ac37cacb4cd1415926d7390548a2f50036485
Ben Naccarato <bennaccarato@improbable.io>
2019-05-13 18:50:07 +0100

    Uncomment out build linux worker

--- BuildProject.bat
+++ BuildProject.bat
@@ -1,6 +1,6 @@
 @echo off
 call "%~dp0ProjectPaths.bat"
-:: call %~dp0%PROJECT_PATH%\"Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat" %GAME_NAME%Server Linux Development %GAME_NAME%.uproject || goto :error
+call %~dp0%PROJECT_PATH%\"Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat" %GAME_NAME%Server Linux Development %GAME_NAME%.uproject || goto :error
 call %~dp0%PROJECT_PATH%\"Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat" %GAME_NAME% Win64 Development %GAME_NAME%.uproject || goto :error
 echo All builds succeeded.
 
