45061f095b410848feba10c9aab29a36006bee1a
Joshua Huburn <joshuahuburn@improbable.io>
2019-05-30 16:31:24 +0100

    Working prototype

--- LaunchSpatial.bat
+++ LaunchSpatial.bat
@@ -1,13 +1,12 @@
 @echo off
-echo Starting a local Spatial deployment with the default launch configuration. Modify LaunchSpatial.bat if you want to use a different launch configuration.
 
-pushd "%~dp0\spatial"
+pushd %~dp0
 
-spatial worker build build-config 
+call %~dp0Game\Plugins\UnrealGDK\SpatialGDK\Build\Scripts\BuildProto.bat %~dp0spatial\schema %~dp0spatial\build\dependencies\schema\standard_library\ %~dp0spatial\build\descriptor\output\schema.descriptor
 
-rem To launch a local Spatial deployment with a different launch configuration, change the default_launch.json argument to the name of your configuration file, 
-rem for example one_worker_test.json or two_worker_test.json. Similarly, you can specify a different snapshot than the default one by replacing the value of 
-rem the --snapshot argument with the path to the snapshot you want to launch with.
-spatial local launch default_launch.json --snapshot=snapshots/default.snapshot
+spatial alpha local launch --main_config=spatial\spatialos.json  --launch_config=spatial\deployment.json --log_directory=spatial\logs
 
 popd
+
+pause
+
