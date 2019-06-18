3855416af9f2ec913a0e83a337be1a85864ae03e
Peter Griffin <petergriffin@improbable.io>
2019-06-18 14:01:10 +0100

    Remove use of auto

--- Game/Source/GDKShooter/Private/Characters/GDKTopDownShooterCharacter.cpp
+++ Game/Source/GDKShooter/Private/Characters/GDKTopDownShooterCharacter.cpp
@@ -4,7 +4,7 @@
 
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "GDKLogging.h"v
+#include "GDKLogging.h"
 
 AGDKTopDownShooterCharacter::AGDKTopDownShooterCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -46,9 +46,9 @@ void AGDKTopDownShooterCharacter::MoveForwardRight(float Value)
 		float OffsetYaw = TopDownCamera->GetComponentRotation().Yaw;
 		if (bMinus) OffsetYaw -= this->GetControlRotation().Yaw;
 		if (bAdd) OffsetYaw += this->GetControlRotation().Yaw;
-		auto CameraYaw = FRotator(0, OffsetYaw, 0);
-		auto ForwardVector = CameraYaw.RotateVector(FVector::ForwardVector);
-		auto RightVector = CameraYaw.RotateVector(FVector::RightVector);
+		FRotator CameraYaw = FRotator(0, OffsetYaw, 0);
+		FVector ForwardVector = CameraYaw.RotateVector(FVector::ForwardVector);
+		FVector RightVector = CameraYaw.RotateVector(FVector::RightVector);
 		AddMovementInput(ForwardVector, FMath::Abs(Value));
 		AddMovementInput(RightVector, Value);
 	}
@@ -61,8 +61,8 @@ void AGDKTopDownShooterCharacter::MoveForward(float Value)
 		float OffsetYaw = TopDownCamera->GetComponentRotation().Yaw;
 		if (bMinus) OffsetYaw -= this->GetControlRotation().Yaw;
 		if (bAdd) OffsetYaw += this->GetControlRotation().Yaw;
-		auto CameraYaw = FRotator(0, OffsetYaw, 0);
-		auto ForwardVector = CameraYaw.RotateVector(FVector::ForwardVector);
+		FRotator CameraYaw = FRotator(0, OffsetYaw, 0);
+		FVector ForwardVector = CameraYaw.RotateVector(FVector::ForwardVector);
 		AddMovementInput(ForwardVector, Value);
 	}
 }
@@ -74,8 +74,8 @@ void AGDKTopDownShooterCharacter::MoveRight(float Value)
 		float offsetYaw = TopDownCamera->GetComponentRotation().Yaw;
 		if (bMinus) offsetYaw -= this->GetControlRotation().Yaw;
 		if (bAdd) offsetYaw += this->GetControlRotation().Yaw;
-		auto CameraYaw = FRotator(0, offsetYaw, 0);
-		auto RightVector = CameraYaw.RotateVector(FVector::RightVector);
+		FRotator CameraYaw = FRotator(0.f, offsetYaw, 0.f);
+		FVector RightVector = CameraYaw.RotateVector(FVector::RightVector);
 		AddMovementInput(RightVector, Value);
 	}
 }
@@ -106,13 +106,13 @@ void AGDKTopDownShooterCharacter::BeginPlay()
 
 float ClampAngle(float Input)
 {
-	while (Input < -180)
+	while (Input < -180.f)
 	{
-		Input += 360;
+		Input += 360.f;
 	}
-	while (Input > 180)
+	while (Input > 180.f)
 	{
-		Input -= 360;
+		Input -= 360.f;
 	}
 	return Input;
 }
