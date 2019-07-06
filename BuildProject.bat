d169a508bb6109c99076e50d12bc63b63a6cca0c
Joshua Huburn <joshuahuburn@improbable.io>
2019-07-06 16:37:56 +0100

    Use hardpath to engine plugin for BuildProject.bat

--- BuildProject.bat
+++ BuildProject.bat
@@ -1,13 +1,30 @@
 @echo off
+
+rem Input args: BuildName, Platform, BuildConfiguration, uproject
+
 call "%~dp0ProjectPaths.bat"
-call %~dp0%PROJECT_PATH%\"Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat" %GAME_NAME%Server Linux Development %GAME_NAME%.uproject || goto :error
-call %~dp0%PROJECT_PATH%\"Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat" %GAME_NAME% Win64 Development %GAME_NAME%.uproject || goto :error
-echo All builds succeeded.
+
+rem Try to build using a project plugin first.
+set BuildWorkerBat="%~dp0%PROJECT_PATH%/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat"
+if exist %BuildWorkerBat% (
+    goto :Build
+)
+
+rem If no project plugin exists, try to build using an engine plugin.
+set BuildWorkerBat="%~dp0../../Engine/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat"
+if not exist %BuildWorkerBat% (
+    echo Cannot find BuildWorker.bat! Please check you have the SpatialGDK plugin installed.
+    goto :Error
+)
+
+:Build
+call %BuildWorkerBat% %GAME_NAME%Server Linux Development %GAME_NAME%.uproject || goto :Error
+call %BuildWorkerBat% %GAME_NAME% Win64 Development %GAME_NAME%.uproject || goto :Error
 
 pause
 exit /b 0
 
-:error
+:Error
 echo Builds failed.
 pause
 exit /b 1
