a2da61aa25648d798e6942d86de4daa104b98e32
peterimprobable <petergriffin@improbable.io>
2019-07-08 18:09:37 +0100

    Revert "Remove previously deleted files"
    This reverts commit 2bcdd3e4da4d7e11e45be71872181bba49d39e77.

---
+++ Game/Source/GDKShooter/Private/Components/ShootingComponent.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "ShootingComponent.h"
+#include "GDKLogging.h"
+
+UShootingComponent::UShootingComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UShootingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TArray<UActorComponent*> AllComponents;
+	GetOwner()->GetComponents<UActorComponent>(AllComponents);
+	bool bHasTraceProvider = false;
+
+	for (int i = 0; i < AllComponents.Num(); i++)
+	{
+		if (AllComponents[i]->GetClass()->ImplementsInterface(UTraceProvider::StaticClass()))
+		{
+			TraceProvider.SetObject(AllComponents[i]);
+			TraceProvider.SetInterface(Cast<ITraceProvider>(AllComponents[i]));
+			bHasTraceProvider = true;
+		}
+	}
+
+	if (!bHasTraceProvider)
+	{
+		UE_LOG(LogGDK, Error, TEXT("Shooting Component Exists without a Trace Providing component, please add an Actor Component that implements ITraceProvider."));
+	}
+}
+
