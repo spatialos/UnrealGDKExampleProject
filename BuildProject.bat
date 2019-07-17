08363345c69dd73a859fa9cb1077a195143703c9
Joshua Huburn <joshuahuburn@improbable.io>
2019-07-17 12:18:01 +0100

    Updated helper scripts to handle spaces in path and fixed Launch scripts

--- BuildProject.bat
+++ BuildProject.bat
@@ -13,65 +13,7 @@ if exist "%~dp0\%PROJECT_PATH%\Plugins\UnrealGDK" (
 )
 
 rem If there is no project plugin. Find the engine plugin.
-
-rem Get the Unreal Engine used by this project by querying the registry for the engine association found in the .uproject.
-set UNREAL_ENGINE=""
-set UPROJECT=""
-
-rem First find the .uproject
-for /f "delims=" %%A in (' powershell -Command "Get-ChildItem %~dp0 -Depth 1 -Filter *.uproject -File | %% {$_.FullName}" ') do set UPROJECT="%%A"
-
-if %UPROJECT%=="" (
-    echo Error: Could not find uproject. Please make sure you have passed in the project directory correctly.
-    pause
-    exit /b 1
-)
-
-echo Using uproject: %UPROJECT%
-
-rem Get the Engine association from the uproject.
-for /f "delims=" %%A in (' powershell -Command "(Get-Content %UPROJECT% | ConvertFrom-Json).EngineAssociation" ') do set ENGINE_ASSOCIATION="%%A"
-
-echo Engine association for uproject is: %ENGINE_ASSOCIATION%
-
-rem If the engine association is a path then use this.
-if exist "%ENGINE_ASSOCIATION%" (
-    set UNREAL_ENGINE=%ENGINE_ASSOCIATION%
-)
-
-rem Try and use the engine association as a key in the registry to get the path to Unreal.
-if %UNREAL_ENGINE%=="" (
-    if not "%ENGINE_ASSOCIATION%"=="" (
-        rem Query the registry for the path to the Unreal Engine using the engine association.
-        for /f "usebackq tokens=3*" %%A in (`reg query "HKCU\Software\Epic Games\Unreal Engine\Builds" /v %ENGINE_ASSOCIATION%`) do (
-            set UNREAL_ENGINE="%%A %%B"
-        )
-    )
-)
-
-rem If there was no engine association then we need to climb the directory path of the project to find the Engine.
-if %UNREAL_ENGINE%=="" (
-    pushd "%~dp0"
-
-    :climb_parent_directory
-    if exist Engine (
-        rem Check for the Build.version file to be sure we have found a correct Engine folder.
-        if exist "Engine\Build\Build.version" (
-            set UNREAL_ENGINE="!cd!"
-        )
-    ) else (
-        rem This checks if we are in a root directory. If so we cannot check any higher and so should error out.
-        if "%cd:~3,1%"=="" (
-            echo Error: Could not find Unreal Engine folder. Please set a project association or ensure your game project is within an Unreal Engine folder.
-            pause
-            exit /b 1
-        )
-        cd ..
-        goto :climb_parent_directory
-    )
-
-    popd
-)
+call "%~dp0FindEngine.bat"
 
 if %UNREAL_ENGINE%=="" (
     echo Error: Could not find the Unreal Engine. Please associate your '.uproject' with an engine version or ensure this game project is nested within an engine build.
@@ -81,7 +23,6 @@ if %UNREAL_ENGINE%=="" (
 
 rem Make the relative path absolute. Pushd to PROJECT_PATH is required as a relative path will be relative to the .uproject file.
 pushd "%~dp0\%PROJECT_PATH%"
-echo Changing engine: %UNREAL_ENGINE%
 cd /d %UNREAL_ENGINE%
 echo Using Unreal Engine at: %cd%
 set GDK_DIRECTORY="%cd%\Engine\Plugins\UnrealGDK"
@@ -91,6 +32,7 @@ popd
 echo Building worker with GDK located at %GDK_DIRECTORY%
 
 call %GDK_DIRECTORY%\SpatialGDK\Build\Scripts\BuildWorker.bat %GAME_NAME%Server Linux Development "%~dp0\%PROJECT_PATH%\%GAME_NAME%.uproject" || goto :error
+call %GDK_DIRECTORY%\SpatialGDK\Build\Scripts\BuildWorker.bat %GAME_NAME%SimulatedPlayer Linux Development "%~dp0\%PROJECT_PATH%\%GAME_NAME%.uproject" || goto :error
 call %GDK_DIRECTORY%\SpatialGDK\Build\Scripts\BuildWorker.bat %GAME_NAME% Win64 Development "%~dp0\%PROJECT_PATH%\%GAME_NAME%.uproject" || goto :error
 echo All builds succeeded.
 
