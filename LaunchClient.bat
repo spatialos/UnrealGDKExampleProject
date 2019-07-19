0d00b70d7d3e5f52fbea9679a31dd28420218f3b
Ieva Vasiljeva <ieva@improbable.io>
2019-07-19 13:29:43 +0100

    Strip path to the engine from quotes before concatenating it with path to editor executable.

--- LaunchClient.bat
+++ LaunchClient.bat
@@ -1,4 +1,8 @@
 @echo off
 call "%~dp0FindEngine.bat"
 call "%~dp0ProjectPaths.bat"
-%UNREAL_ENGINE%"\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0%PROJECT_PATH%\%GAME_NAME%.uproject" 127.0.0.1 -game -log -workerType UnrealClient -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -NoVerifyGC -windowed -ResX=800 -ResY=600
+
+:: Need to strip the path to engine from quotes, so that we can combine it with the path to editor executable.
+set UNREAL_ENGINE_STRIPPED_FROM_QUOTES=%UNREAL_ENGINE:~1,-1%
+
+"%UNREAL_ENGINE_STRIPPED_FROM_QUOTES%\Engine\Binaries\Win64\UE4Editor.exe" "%~dp0%PROJECT_PATH%\%GAME_NAME%.uproject" 127.0.0.1 -game -log -workerType UnrealClient -stdout -nowrite -unattended -nologtimes -nopause -noin -messaging -NoVerifyGC -windowed -ResX=800 -ResY=600
