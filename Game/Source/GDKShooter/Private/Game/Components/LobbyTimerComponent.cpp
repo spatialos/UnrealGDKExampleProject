cd2607e84bc875c19b8c065c27eb921db1186ff4
Tim Gibson <timgibson@improbable.io>
2019-08-01 10:54:41 +0100

    Add cheat for min players to start match

--- Game/Source/GDKShooter/Private/Game/Components/LobbyTimerComponent.cpp
+++ Game/Source/GDKShooter/Private/Game/Components/LobbyTimerComponent.cpp
@@ -11,6 +11,7 @@ void ULobbyTimerComponent::BeginPlay()
 	// Developer cheat so you don't have to wait for a long time before entering a match.
 	const TCHAR* CommandLine = FCommandLine::Get();
 	FParse::Value(CommandLine, TEXT("lobbytime"), DefaultTimerDuration);
+	FParse::Value(CommandLine, TEXT("lobbyminplayers"), MinimumPlayersToStartCountdown);
 #endif
 	bAutoStart = (MinimumPlayersToStartCountdown == 0);
 	Super::BeginPlay();
