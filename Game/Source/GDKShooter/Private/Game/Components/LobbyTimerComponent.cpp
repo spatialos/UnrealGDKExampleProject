05c95b9f85a07c8d9713d3ea9e80d265ef045370
Tim Gibson <timgibson@improbable.io>
2019-08-01 14:49:24 +0100

    Add command line cheats to set lobby and match timers (#39)
    * Add command line cheats to set lobby and match timers (-lobbytimer N and -matchtimer N for N seconds)
    * Add cheat for min players to start match (-lobbyminplayers N)

--- Game/Source/GDKShooter/Private/Game/Components/LobbyTimerComponent.cpp
+++ Game/Source/GDKShooter/Private/Game/Components/LobbyTimerComponent.cpp
@@ -3,9 +3,16 @@
 #include "LobbyTimerComponent.h"
 #include "GDKLogging.h"
 #include "GameFramework/Actor.h"
+#include "Misc/CommandLine.h"
 
 void ULobbyTimerComponent::BeginPlay()
 {
+#if !UE_BUILD_SHIPPING
+	// Developer cheat so you don't have to wait for a long time before entering a match.
+	const TCHAR* CommandLine = FCommandLine::Get();
+	FParse::Value(CommandLine, TEXT("lobbytime"), DefaultTimerDuration);
+	FParse::Value(CommandLine, TEXT("lobbyminplayers"), MinimumPlayersToStartCountdown);
+#endif
 	bAutoStart = (MinimumPlayersToStartCountdown == 0);
 	Super::BeginPlay();
 }
