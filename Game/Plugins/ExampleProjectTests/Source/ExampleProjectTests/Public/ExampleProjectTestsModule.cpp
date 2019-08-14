b793b437885fcebbb43d21b6cce96bd9e6ec0757
Ieva Vasiljeva <ieva@improbable.io>
2019-08-14 10:27:55 +0100

    Remove unused imports.

--- Game/Plugins/ExampleProjectTests/Source/ExampleProjectTests/Public/ExampleProjectTestsModule.cpp
+++ Game/Plugins/ExampleProjectTests/Source/ExampleProjectTests/Public/ExampleProjectTestsModule.cpp
@@ -1,9 +1,5 @@
 #include "ExampleProjectTestsModule.h"
 #include "Modules/ModuleManager.h"
-#include "SpatialAutomationCommon.h"
-//#include "../SpatialAutomationCommon.h"
-#include "Misc/AutomationTest.h"
-#include "SpatialAutomationTest.h"
 
 DEFINE_LOG_CATEGORY(LogSpatialAutomationTest);
 
