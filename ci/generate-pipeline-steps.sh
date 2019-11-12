8f10b3a0c1b0a169543f155dac218f6fa6c03166
Miron Zelina <mironzelina@improbable.io>
2019-11-12 14:57:55 +0000

    Rename BUILDKITE_SLACK_NOTIFY to SLACK_NOTIFY

--- ci/generate-pipeline-steps.sh
+++ ci/generate-pipeline-steps.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # Download the unreal-engine.version file from the GDK repo so we can run the example project builds on the same versions the GDK was run against
 GDK_BRANCH_LOCAL="${GDK_BRANCH:-master}"
 curl https://raw.githubusercontent.com/spatialos/UnrealGDK/$GDK_BRANCH_LOCAL/ci/unreal-engine.version -o ci/unreal-engine.version
-BUILDKITE_SLACK_NOTIFY_LOCAL="${BUILDKITE_SLACK_NOTIFY:-true}"
 
 # This script generates steps for each engine version listed in unreal-engine.version in the GDK, and adds those to generated_base.steps.yaml
 # The steps are based on the template in nightly.template.steps.yaml
@@ -16,11 +15,6 @@ if [ -z "${ENGINE_VERSION}" ]; then
     IFS=$'\n'
     for commit_hash in $(cat < ci/unreal-engine.version); do
         REPLACE_STRING="s|ENGINE_COMMIT_HASH_PLACEHOLDER|$commit_hash|g; s|STEP_NUMBER_PLACEHOLDER|$STEP_NUMBER|g"
-        if [ STEP_NUMBER -eq 1 ]; then
-            REPLACE_STRING="$REPLACE_STRING; s|BUILDKITE_SLACK_NOTIFY_PLACEHOLDER|$BUILDKITE_SLACK_NOTIFY_LOCAL|g"
-        else
-            REPLACE_STRING="$REPLACE_STRING; s|BUILDKITE_SLACK_NOTIFY_PLACEHOLDER|false|g"
-        fi
         sed $REPLACE_STRING ci/nightly.template.steps.yaml | buildkite-agent pipeline upload
         STEP_NUMBER=STEP_NUMBER+1
     done
