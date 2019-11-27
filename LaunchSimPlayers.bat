5c7382e8bf1b5a8b2493c1a28c8ec38457e3b070
Matthew Sandford <matthewsandford@improbable.io>
2019-11-27 10:01:24 +0000

    [UNR-2425][MS] As part of a bugfix I thought it would be useful to include these scripts.

---
+++ LaunchSimPlayers.bat
@@ -0,0 +1,11 @@
+@echo off
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
+start LaunchSimPlayerClient.bat
