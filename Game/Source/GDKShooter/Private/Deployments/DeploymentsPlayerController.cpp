1ca89b5eb83ef91401f79059dadad70a8a1b93d8
raymonwang <raymonwang@improbable.io>
2020-02-04 10:53:54 +0800

    Fix format issue.

--- Game/Source/GDKShooter/Private/Deployments/DeploymentsPlayerController.cpp
+++ Game/Source/GDKShooter/Private/Deployments/DeploymentsPlayerController.cpp
@@ -12,17 +12,17 @@
 void ADeploymentsPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	ActivateTouchInterface(nullptr);
 	bShowMouseCursor = true;
-	
+
 	auto SpatialGameInstance = GetGameInstance<USpatialGameInstance>();
 	auto SpatialWorkerConnection = SpatialGameInstance->GetSpatialWorkerConnection();
 	if (!SpatialWorkerConnection) {
 		UE_LOG(LogGDK, Log, TEXT("Failure: failed to get SpatialWorkerConnection"));
 		return;
 	}
-	
+
 	auto SpatialWorkerType = SpatialGameInstance->GetSpatialWorkerType().ToString();
 	// Register a callback function into SpatialWorkerConnection so it can trigger a custom function written by user when it receive login token from spatial cloud.
 	SpatialWorkerConnection->RegisterOnLoginTokensCallback(std::bind(&ADeploymentsPlayerController::Populate, this, std::placeholders::_1));
@@ -40,11 +40,11 @@ void ADeploymentsPlayerController::EndPlay(const EEndPlayReason::Type Reason)
 FDeploymentInfo Parse(const Worker_Alpha_LoginTokenDetails LoginToken)
 {
 	FDeploymentInfo DeploymentInfo;
-	
+
 	DeploymentInfo.DeploymentId = UTF8_TO_TCHAR(LoginToken.deployment_id);
 	DeploymentInfo.DeploymentName = UTF8_TO_TCHAR(LoginToken.deployment_name);
 	DeploymentInfo.LoginToken = UTF8_TO_TCHAR(LoginToken.login_token);
-	
+
 	for (int i = 0; i < (int)LoginToken.tag_count; i++)
 	{
 		FString tag = UTF8_TO_TCHAR(LoginToken.tags[i]);
@@ -72,13 +72,13 @@ bool ADeploymentsPlayerController::Populate(const Worker_Alpha_LoginTokensRespon
 	{
 		DeploymentArray.Add(Parse(Deployments->login_tokens[i]));
 	}
-	
-	
+
+
 	DeploymentArray.Sort([](const FDeploymentInfo& lhs, const FDeploymentInfo& rhs)
 						 {
 		return lhs.DeploymentName.Compare(rhs.DeploymentName) < 0;
 	});
-	
+
 	OnDeploymentsReceived.Broadcast(DeploymentArray);
 	return true;
 }
@@ -91,16 +91,16 @@ void ADeploymentsPlayerController::JoinDeployment(const FString& LoginToken)
 		UE_LOG(LogGDK, Log, TEXT("Failure: failed to get SpatialWorkerConnection"));
 		return;
 	}
-	
+
 	const auto& DevAuthConfig = SpatialWorkerConnection->DevAuthConfig;
 	FURL TravelURL;
 	TravelURL.Host = DevAuthConfig.LocatorHost;
 	TravelURL.AddOption(TEXT("locator"));
 	TravelURL.AddOption(*FString::Printf(TEXT("playeridentity=%s"), *DevAuthConfig.PlayerIdentityToken));
 	TravelURL.AddOption(*FString::Printf(TEXT("login=%s"), *LoginToken));
-	
+
 	OnLoadingStarted.Broadcast();
-	
+
 	ClientTravel(TravelURL.ToString(), TRAVEL_Absolute, false);
 }
 
