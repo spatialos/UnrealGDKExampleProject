b2d42ec75ab4d880879f495b01272e24511c019c
Matthew Sandford <matthewsandford@improbable.io>
2020-03-31 10:12:00 +0100

    Revert "Remove old cross server pawn. We have now added cross server rpcs to actor. (#127)"
    This reverts commit a74f080b683d98c58b863c3039cef44e1efb8890.

---
+++ Game/Source/GDKShooter/Private/GameFramework/CrossServerPawn.cpp
@@ -0,0 +1,15 @@
+ // Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#include "GameFramework/CrossServerPawn.h"
+
+float ACrossServerPawn::TakeDamage(float Damage, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	TakeDamageCrossServer(Damage, DamageEvent, nullptr, DamageCauser);
+	return Damage;
+}
+
+void ACrossServerPawn::TakeDamageCrossServer_Implementation(float Damage, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float ActualDamage = Super::TakeDamage(Damage, DamageEvent, EventInstigator, DamageCauser);
+	IncomingDamage.Broadcast(ActualDamage, DamageEvent, EventInstigator, DamageCauser);
+}
