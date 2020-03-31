c75a4b5f52006f7410d0960ccd98d6cd901c8329
MatthewSandfordImprobable <matthewsandford@improbable.io>
2020-03-31 10:30:54 +0100

    [UNR-3194][MS] Adding back cross server pawn. (#131)
    * Moving and renaming config setting for UNR-3142
    
    * Revert "Remove old cross server pawn. We have now added cross server rpcs to actor. (#127)"
    
    This reverts commit a74f080b683d98c58b863c3039cef44e1efb8890.
    
    * Revert "Moving and renaming config setting for UNR-3142"
    
    This reverts commit 9f80e42e8a4248a380a7d5639374bbff33a2339f.

---
+++ Game/Source/GDKShooter/Public/GameFramework/CrossServerPawn.h
@@ -0,0 +1,25 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CrossServerPawn.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FIncomingDamageEvent, float, Damage, const struct FDamageEvent&, DamageEvent, AController*, EventInstigator, AActor*, DamageCauser);
+
+UCLASS()
+class GDKSHOOTER_API ACrossServerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+		FIncomingDamageEvent IncomingDamage;
+
+	float TakeDamage(float Damage, const struct FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
+
+	UFUNCTION(CrossServer, Reliable)
+		void TakeDamageCrossServer(float Damage, const struct FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
+	
+};
