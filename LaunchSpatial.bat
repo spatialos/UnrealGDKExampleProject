fc8ae4332bff0cd801ba2c9bc0e7a1de4346fcce
Miron Zelina <mironzelina@improbable.io>
2020-06-24 11:55:00 +0100

    [UNR-3719] Fix runtime version in batch scripts (#184)

--- LaunchSpatial.bat
+++ LaunchSpatial.bat
@@ -8,6 +8,6 @@ spatial worker build build-config
 rem To launch a local Spatial deployment with a different launch configuration, change the default_launch.json argument to the name of your configuration file, 
 rem for example one_worker_test.json or two_worker_test.json. Similarly, you can specify a different snapshot than the default one by replacing the value of 
 rem the --snapshot argument with the path to the snapshot you want to launch with.
-spatial local launch default_launch.json --snapshot=snapshots/default.snapshot
+spatial local launch default_launch.json --snapshot=snapshots/default.snapshot --runtime_version=0.4.1
 
 popd
