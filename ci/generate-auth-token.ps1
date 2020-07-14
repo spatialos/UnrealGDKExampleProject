4402763d89078389fb9cfbd6342d808b7128d8c2
KenYu <kenyu@improbable.io>
2020-07-14 18:42:53 +0800

    spatial update before gen auth token

--- ci/generate-auth-token.ps1
+++ ci/generate-auth-token.ps1
@@ -12,17 +12,20 @@ $gdk_home = "${exampleproject_home}\Game\Plugins\UnrealGDK"
 pushd "$exampleproject_home"
     pushd "spatial"
         Start-Event "generate-auth-token" "deploy-unreal-gdk-example-project-:windows:"
-        $DESCRIPTION = "Unreal-GDK-Token" 
-        $DEVAUTH_CREATE = spatial project auth dev-auth-token create --description=$DESCRIPTION --project_name=$project_name | Out-String
-        Write-Output $DEVAUTH_CREATE
-        $FOUND_DEV_TOKEN = $DEVAUTH_CREATE -match 'token_secret:\\"(.+)\\"'
-        if ($FOUND_DEV_TOKEN -eq 0) {
-            Write-Output "Failed to find dev auth token"
-            Throw "dev auth token not found"
-        }
-        $auth_token = $matches[1]
-        Set-Meta-Data -variable_name "auth-token" "$auth_token"
-        Write-Output "auth_token:$auth_token"
+            Write-Output "spatial update"
+            spatial update
+
+            $DESCRIPTION = "Unreal-GDK-Token" 
+            $DEVAUTH_CREATE = spatial project auth dev-auth-token create --description=$DESCRIPTION --project_name=$project_name | Out-String
+            Write-Output $DEVAUTH_CREATE
+            $FOUND_DEV_TOKEN = $DEVAUTH_CREATE -match 'token_secret:\\"(.+)\\"'
+            if ($FOUND_DEV_TOKEN -eq 0) {
+                Write-Output "Failed to find dev auth token"
+                Throw "dev auth token not found"
+            }
+            $auth_token = $matches[1]
+            Set-Meta-Data -variable_name "auth-token" "$auth_token"
+            Write-Output "auth_token:$auth_token"
         Finish-Event "generate-auth-token" "deploy-unreal-gdk-example-project-:windows:"    
     popd
 
