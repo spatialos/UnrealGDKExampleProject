ded5b9500cff54c0da3b55ab66985684635ecdf2
KenYu <kenyu@improbable.io>
2020-07-14 19:12:13 +0800

    add debug infor

--- ci/generate-auth-token.ps1
+++ ci/generate-auth-token.ps1
@@ -9,10 +9,19 @@ $project_name = Get-Env-Variable-Value-Or-Default -environment_variable_name "SP
 
 $gdk_home = "${exampleproject_home}\Game\Plugins\UnrealGDK"
 Start-Event "clone-gdk-plugin" "build-unreal-gdk-example-project-:windows:"
+    Write-Output "gdk_repo:$gdk_repo"
+    Write-Output "gdk_branch_name:$gdk_branch_name"
+    Write-Output "project_name:$project_name"
+
     pushd "Game"
         New-Item -Name "Plugins" -ItemType Directory -Force
         pushd "Plugins"
-            git clone $gdk_repo --depth 1 -b "$gdk_branch_name"
+            Start-Process -Wait -PassThru -NoNewWindow -FilePath "git" -ArgumentList @(`
+                "clone", `
+                "$gdk_repo", `
+                "--depth 1", `
+                "-b $gdk_branch_name"
+            )
         popd
     popd
 Finish-Event "clone-gdk-plugin" "build-unreal-gdk-example-project-:windows:"
