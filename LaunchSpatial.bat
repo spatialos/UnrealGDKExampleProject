030db48af76903921b1484fe95e5621c868aaa95
yunjielu <yunjielu@improbable.io>
2021-04-09 13:52:11 +0800

    commit AI movement implementation

--- LaunchSpatial.bat
+++ LaunchSpatial.bat
@@ -8,6 +8,6 @@ spatial worker build build-config
 rem To launch a local Spatial deployment with a different launch configuration, change the default_launch.json argument to the name of your configuration file, 
 rem for example one_worker_test.json or two_worker_test.json. Similarly, you can specify a different snapshot than the default one by replacing the value of 
 rem the --snapshot argument with the path to the snapshot you want to launch with.
-spatial local launch default_launch.json --snapshot=snapshots/default.snapshot --runtime_version=15.0.1
+spatial local launch C:\git\UE4_426\Samples\UnrealGDKExampleProject\spatial\one_worker_test.json --snapshot=snapshots/default.snapshot --runtime_version=15.0.1
 
 popd
