b6ffd1a956ce16fdf0f2d0ae44e00a11a26288f1
Danny Birch <danielbirch@improbable.io>
2021-05-28 16:13:26 +0100

    check can be proven by analysis

--- Game/Source/GDKShooter/Public/UI/TouchControls.h
+++ Game/Source/GDKShooter/Public/UI/TouchControls.h
@@ -30,8 +30,7 @@ struct SpeedStatistics
 		if (MoveStatisticsCache.IsFull())
 		{
 			float OldestOffset;
-			bool bResult = MoveStatisticsCache.Dequeue(OldestOffset);
-			check(bResult);
+			MoveStatisticsCache.Dequeue(OldestOffset);
 			OffsetNum -= OldestOffset;
 		}
 		MoveStatisticsCache.Enqueue(Offset);
